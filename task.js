c334d5050dd6b6ea36e62d4afcf722db60933b3b
anhnhivu <56817939+anhnhivu@users.noreply.github.com>
2020-09-05 15:12:34 +0700

    Update task.js

--- task.js
+++ task.js
@@ -5,28 +5,3 @@ function greet() {
     }
         document.querySelector('h1').innerHTML = 'Hello, ' + name + '!';
 };
-
-/*var upload = function(event) {
-	var image = document.getElementById("output");
-	image.src = URL.createObjectURL(event.target.files[0]);
-};
-
-firebase.initializeApp(firebaseConfig);
-console.log(firebase)
-
-function uploadeImage() {
-    const ref = firebase.storage().ref()
-    const file = document.querySelector("#photo").files[0]
-    const name = new Date() + '-' + file.name
-    const metadata = {
-        contentType:file.type
-    }
-    const task = ref.child(name).put(file,metadata)
-    task
-    .then(snapshot => snapshot.ref.getDownloadURL())
-    .then(url => {
-        console.log(url)
-        alert("Image Upload Successful")
-        const image = 
-    })
-};*/
