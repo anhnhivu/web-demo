bbbc2cfa048e3c2d5d955fd717d0caa3086a7ee4
anhnhivu <56817939+anhnhivu@users.noreply.github.com>
2020-09-07 16:17:18 +0700

    Delete task.js

--- task.js
+++
@@ -1,7 +0,0 @@
-function greet() {
-    let name = document.querySelector('#crs').value;
-    if (name === '') {
-        name = ' ';
-    }
-        document.querySelector('h1').innerHTML = 'Hello, ' + name + '!';
-};
